f1dd52f99b89aadf627c226b2eefaee2c0c3c731
KizKnife <134030696+KizKnife@users.noreply.github.com>
2026-05-26 09:19:24 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/MainProgram.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import com.pluralsight.ui.UserInterface;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+
+    }
+}
